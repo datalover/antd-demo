8c72933e6a0c108a81bde5b867e7d693f2dc9495
Yanyu Qu <yanyu.qu@revera.co.nz>
2019-09-25 15:53:57 +1200

    add more pages

--- config/config.js
+++ config/config.js
@@ -1,7 +1,33 @@
 export default {
     singular: true,
+    plugins: [
+        ['umi-plugin-react', {
+            antd: true,
+            dva: true
+        }],
+
+    ],
     routes: [{
         path: '/',
-        component: './HelloWorld',
+        component: '../layout',
+        routes: [
+            {
+                path: '/',
+                component: 'Helloworld'
+            },
+            {
+                path: '/helloworld',
+                component: './HelloWorld'
+            },
+            {
+                path: '/dashboard',
+                routes: [
+                    { path: '/dashboard/analysis', component: 'Dashboard/Analysis' },
+                    { path: '/dashboard/monitor', component: 'Dashboard/Monitor' },
+                    { path: '/dashboard/workplace', component: 'Dashboard/Workplace' }
+                ]
+            },
+            { path: '/puzzlecards', component: './puzzlecards'}
+        ]
     }]
 }
